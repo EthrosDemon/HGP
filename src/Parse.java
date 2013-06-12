990044bf292e6df687ba756c74ec0ab7061ab6e3
EthrosDemon <crazyenzyme@gmail.com>
2013-06-12 10:50:02 +0200

    deleted commented lines

--- src/Parse.java
+++ src/Parse.java
@@ -31,27 +31,6 @@ public class Parse {
 		out.print(str);
 		out.close();
 
-		// String sep = System.getProperty("file.separator");
-		// System.out.println("foo" + sep + "works");
-		// while (true) {
-		// BufferedReader read2 = new BufferedReader(
-		// new InputStreamReader(new FileInputStream("chr22.fa"), "UTF-8"));
-		// System.out.println(read2);
-		// PrintWriter out = new PrintWriter("out.txt");
-		// str = (str + (space * n + space) + ", ");
-		// n++;
-		// Thread.sleep(0);
-		// if (n % 20 == 0) {
-		// str = (str + "\n");
-		// }
-		// if (n % (end) == 0) {
-		// System.out.println(str);
-		// out.print(str);
-		// out.close();
-		// break;
-		// }
-		// }
-
 	}
 
 }
